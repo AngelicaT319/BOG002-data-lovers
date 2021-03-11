c1a7eae610603228eddc0a7c2414cbb276d23b1f
carfolacam98 <laura.carfolacam@gmail.com>
2021-03-11 12:21:47 -0500

    adelanticos en el main

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
+//aqui lo del DOM xd
 import { example } from './data.js';
-import data from './data/lol/lol.js';
-const aver=data.data.Aatrox.blurb;
-example(aver)
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import datos from './data/lol/lol.js';
+const aver=datos.data;
 
-console.log( aver);
+
+var filtro1= function(nombrecampeon){
+    for (var i in aver){
+        console.log(aver[i].id);
+        document.getElementById('root').innerHTML+=aver[i].id;
+        
+        /*if (nombrecampeon===i){
+            return aver[i];
+
+        }*/
+    }
+
+}
+example(aver)
+
+
+filtro1('Aatrox');
+
+//for(var i in filtro1('Aatrox')){
+    //console.log(filtro1('Aatrox')[i]);
+   // if ('info'===i){
+        //console.log(filtro1('Aatrox')[i].attack);
+
+//}
+//}
+/*console.log( aver);
+console.log(typeof aver)
+console.log(Object.keys(aver))*/
