3f1651f99c8d73bba290f65a9e201445ea36fa33
Angelica <atheran082@gmail.com>
2021-03-16 11:37:46 -0500

    muestra de campeones en html

--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
-//export function DataCampeones () 
+export function DataCampeones () {
+    
+}
       
               
              
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 
 //Aqui lo del DOM
-//import data from './data/lol/lol.js';
+import data from './data/lol/lol.js';
 
-//import {DataCampeones} from  './data.js';
+import {DataCampeones} from  './data.js';
 
 const contenido = document.getElementById('contenido');
 
 //Funcion para buscar campeones
+for (var campeones in data.data){
+    const campeon = (data.data[campeones])
+    var div = document.createElement("div");
+    var name = document.createElement("h3");
+    var imagenCampeon = document.createElement("img");
+    imagenCampeon.src = campeon.img;
+    name.innerText = campeon.name
+    div.appendChild(name)
+    div.appendChild(imagenCampeon)
+    contenido.appendChild(div);
+}
