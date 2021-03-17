3dd1456e05a511558b71a79d0fa061a503e903d0
Angelica <atheran082@gmail.com>
2021-03-17 12:30:27 -0500

    cambio de rama

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Aqui lo del DOM
 import data from './data/lol/lol.js';
 
-//import {DataCampeones} from  './data.js';
+import {DataCampeones} from  './data.js';
 
 const contenido = document.getElementById('contenido');
 
