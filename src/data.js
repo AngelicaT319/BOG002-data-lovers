60c9774276cb9efdd98ac4495e84e94d7ad361b5
carfolacam98 <laura.carfolacam@gmail.com>
2021-03-19 11:48:14 -0500

    funcionando en el momento

--- src/data.js
+++ src/data.js
@@ -1,4 +1,14 @@
-//export function DataCampeones () {
-    
-//}
+export function buscarCampeones (input,selector) {
+    document.addEventListener("keyup",(e) =>{
+        if(e.target.matches(input)){
+           console.log(e.target.value);
+            
+            document.querySelectorAll(selector).forEach((element) => 
+             element.textContent.toLowerCase().includes(e.target.value)
+            ?element.classList.remove("filter")
+            :element.classList.add("filter")
+            );            
+        }
+    });    
+}
       
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-//Aqui lo del DOM
 import data from './data/lol/lol.js';
 
-//import {DataCampeones} from  './data.js';
+import {buscarCampeones} from  './data.js';
 
 const contenido = document.getElementById('contenido');
 
@@ -9,22 +8,48 @@ const contenido = document.getElementById('contenido');
 //genera reflow
 for (var campeones in data.data){
     const campeon = (data.data[campeones])
-   
     var div = document.createElement("div");
     var name = document.createElement("h3");
     var imagenCampeon = document.createElement("img");
-    name.className='namechamp';
-    imagenCampeon.className='imgchamp';
-    div.className='cards';
+    div.className='card';
+     
+    
+    /*name.className='card';
+    imagenCampeon.className='card';*/
+
     imagenCampeon.src = campeon.img;
     name.innerText = campeon.name
     div.appendChild(name)
     div.appendChild(imagenCampeon)
     contenido.appendChild(div);
-    
-
-    div.onclick = function(){
-        alert("yo que se");
-     }
-    
 }
+  
+buscarCampeones(".card-filter", ".card");
+
+const botonabrir = document.getElementById('menudesple');
+botonabrir.addEventListener('mouseover',abrir1);
+botonabrir.addEventListener('mouseout',cerrar1);
+
+const posicion = document.getElementById('posiciones');
+posicion.addEventListener('mouseover',abrir2);
+posicion.addEventListener('mouseout',cerrar2);
+function abrir1(){
+    var elem=document.getElementById('desplegables');
+    elem.style.display = "block";
+   }
+   
+   function cerrar1(){
+    var elem=document.getElementById('desplegables');
+    elem.style.display = "none";
+   }
+   
+   
+   function abrir2(){
+    var elem=document.getElementById('subitem1');
+    elem.style.display = "block";
+   }
+   
+   function cerrar2(){
+    var elem=document.getElementById('subitem1');
+    elem.style.display = "none";
+   }
