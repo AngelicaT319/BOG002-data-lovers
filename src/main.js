c55bcc4807a618328206d90fc3d33935ec5b9d28
carfolacam98 <laura.carfolacam@gmail.com>
2021-03-19 10:13:41 -0500

    responsive-menu-adelantotarjeta

--- src/main.js
+++ src/main.js
@@ -9,18 +9,22 @@ const contenido = document.getElementById('contenido');
 //genera reflow
 for (var campeones in data.data){
     const campeon = (data.data[campeones])
+   
     var div = document.createElement("div");
     var name = document.createElement("h3");
     var imagenCampeon = document.createElement("img");
-    div.className='inline-block';
-    
-    
-    /*name.className='inline-block';
-    imagenCampeon.className='inline-block';*/
-
+    name.className='namechamp';
+    imagenCampeon.className='imgchamp';
+    div.className='cards';
     imagenCampeon.src = campeon.img;
     name.innerText = campeon.name
     div.appendChild(name)
     div.appendChild(imagenCampeon)
     contenido.appendChild(div);
+    
+
+    div.onclick = function(){
+        alert("yo que se");
+     }
+    
 }
