0a990771f4f1cc465b4df3b3b1a50f0c16a1f2ac
Angelica <atheran082@gmail.com>
2021-03-20 15:01:19 -0500

    empieza maquetacion de tarjetas

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 export function buscarCampeones (input,selector) {
     document.addEventListener("keyup",(e) =>{
         if(e.target.matches(input)){
-           console.log(e.target.value);
+           
             
             document.querySelectorAll(selector).forEach((element) => 
              element.textContent.toLowerCase().includes(e.target.value)
