275358d5187b68f356ea9bdac8c93ee41dc7f745
Angelica <atheran082@gmail.com>
2021-03-24 18:07:26 -0500

    agregado de ventana modal o popup

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@
 export function buscarCampeones (input,selector) {
     document.addEventListener("keyup",(e) =>{
         if(e.target.matches(input)){
-           console.log(e.target.value);
+           //console.log(e.target.value);
             
             document.querySelectorAll(selector).forEach((element) => 
              element.textContent.toLowerCase().includes(e.target.value)//para que no descrimine si hay minisculas o mayuscula al escribir
@@ -13,4 +13,12 @@ export function buscarCampeones (input,selector) {
         }
     });    
 }
-      
+   
+export function filtrorol (selector,tank) {
+    console.log( typeof entrandotanque);
+    document.querySelectorAll(selector).forEach((element) => 
+     tank.includes(element.textContent ) 
+     ?element.classList.remove("filter")
+     :element.classList.add("filter")
+     )
+    }
--- src/main.js
+++ src/main.js
@@ -1,55 +1,112 @@
-import data from './data/lol/lol.js';
+import data from "./data/lol/lol.js";
 
-import {buscarCampeones} from  './data.js';
+import { buscarCampeones } from "./data.js";
 
-const contenido = document.getElementById('cartas');
+import {filtrorol} from  './data.js';
 
-//Funcion para buscar campeones
+const contenido = document.getElementById("cartas");
+
+//Funcion para mostrar campeones
 //genera reflow
-for (var campeones in data.data){
-    const campeon = (data.data[campeones])
-    var div = document.createElement("div");
-    var name = document.createElement("h3");
-    var imagenCampeon = document.createElement("img");
-    div.className='card';
-     
-    
-    /*name.className='card';
-    imagenCampeon.className='card';*/
+for (var campeones in data.data) {
+  const campeon = data.data[campeones];
+  const div = document.createElement("div");
+  const name = document.createElement("h3");
+  const imagenCampeon = document.createElement("img");
+  div.className = "card";
 
-    imagenCampeon.src = campeon.img;
-    name.innerText = campeon.name;
-    div.appendChild(name)
-    div.appendChild(imagenCampeon)
-    contenido.appendChild(div);
+  imagenCampeon.src = campeon.img;
+  name.innerText = campeon.name;
+  div.appendChild(name);
+  div.appendChild(imagenCampeon);
+  div.onclick = function(){
+    mostrarPopup(campeon);
+};
+
+  contenido.appendChild(div);
+
 }
-  
+
+//ventana emergente al seleccionar campeon para mirar caracteristicas
+function mostrarPopup (campeon){
+    const contenido = document.getElementById("popup");
+    const name = document.createElement("h1");
+    const div = document.createElement("div");    
+    div.id=('popupdiv');   
+    name.innerText = campeon.id 
+    div.appendChild(name);
+    contenido.appendChild(div);
+    }
+
+    
+    //function cerrarPopup (){
+        //const contenido = document.getElementById("popup");             
+      //contenido.innerHTML = '';       
+    
+    //}
+
+
+//Funcion donde usamos la barra de busqueda para hallar al campeon especifico
 buscarCampeones(".card-filter", ".card");
 
-const botonabrir = document.getElementById('menudesple');
-botonabrir.addEventListener('mouseover',abrir1);
-botonabrir.addEventListener('mouseout',cerrar1);
 
-const posicion = document.getElementById('posiciones');
-posicion.addEventListener('mouseover',abrir2);
-posicion.addEventListener('mouseout',cerrar2);
-function abrir1(){
-    var elem=document.getElementById('desplegables');
-    elem.style.display = "block";
-   }
-   
-   function cerrar1(){
-    var elem=document.getElementById('desplegables');
-    elem.style.display = "none";
-   }
-   
-   
-   function abrir2(){
-    var elem=document.getElementById('subitem1');
-    elem.style.display = "block";
-   }
-   
-   function cerrar2(){
-    var elem=document.getElementById('subitem1');
-    elem.style.display = "none";
-   }
+//Funcion para filtrar por roles y demas a los campeones
+var roles = document.getElementsByClassName("roles");
+var roleslista = "";
+for (let i = 0; i < roles.length; i++) {
+  roles[i].addEventListener("click", () => {
+    if (roles[i].textContent == "Apoyo") {
+      roleslista = "Support";
+    } else if (roles[i].textContent == "Asesino") {
+      roleslista = "Assassin";
+    } else if (roles[i].textContent == "Luchador") {
+      roleslista = "Fighter";
+    } else if (roles[i].textContent == "Tirador") {
+      roleslista = "Marksman";
+    } else if (roles[i].textContent == "Mago") {
+      roleslista = "Mage";
+    } else if (roles[i].textContent == "Tanque") {
+      roleslista = "Tank";
+    }
+    var listaderoles = [];
+    for (var rep in data.data) {
+      console.log(data.data[rep].tags);
+      //includes verifica si dentro de un array hay un elemento
+      //que tenga la palabra que le meti xd
+      if (data.data[rep].tags.includes(roleslista)) {
+        //console.log(data.data[rep].name);
+        listaderoles.push(data.data[rep].name);
+      }
+    }
+
+    filtrorol (".card", listaderoles);
+  });
+}
+
+
+const botonabrir = document.getElementById("menudesple");
+botonabrir.addEventListener("mouseover", abrir1);
+botonabrir.addEventListener("mouseout", cerrar1);
+
+const posicion = document.getElementById("posiciones");
+posicion.addEventListener("click", abrir2);
+posicion.addEventListener("click", cerrar2);
+function abrir1() {
+  var elem = document.getElementById("desplegables");
+  elem.style.display = "block";
+}
+
+function cerrar1() {
+  var elem = document.getElementById("desplegables");
+  elem.style.display = "none";
+}
+
+function abrir2() {
+  var elem = document.getElementById("subitem1");
+  elem.style.display = "block";
+}
+
+function cerrar2() {
+  var elem = document.getElementById("subitem1");
+  elem.style.display = "none";
+}
