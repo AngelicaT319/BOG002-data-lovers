42270b7b3d550257192ba7e5a58f42eb16468770
Angelica <atheran082@gmail.com>
2021-03-29 19:12:52 -0500

    cambios varios

--- src/data.js
+++ src/data.js
@@ -1,24 +1,24 @@
-
 //Funcion para la barra de busqueda//
-export function buscarCampeones (input,selector) {
-    document.addEventListener("keyup",(e) =>{
-        if(e.target.matches(input)){
-           //console.log(e.target.value);
-            
-            document.querySelectorAll(selector).forEach((element) => 
-             element.textContent.toLowerCase().includes(e.target.value)//para que no descrimine si hay minisculas o mayuscula al escribir
-            ?element.classList.remove("filter")
-            :element.classList.add("filter")
-            );            
-        }
-    });    
-}
-   
-export function filtrorol (selector,tank) {
-    //console.log( typeof entrandotanque);
-    document.querySelectorAll(selector).forEach((element) => 
-     tank.includes(element.textContent ) 
-     ?element.classList.remove("filter")
-     :element.classList.add("filter")
-     )
+export function buscarCampeones(input, selector) {
+  document.addEventListener("keyup", (e) => {
+    if (e.target.matches(input)) {
+      //console.log(e.target.value);
+      document.querySelectorAll(selector).forEach((element) =>
+        element.textContent.toLowerCase().includes(e.target.value) //para que no descrimine si hay minisculas o mayuscula al escribir
+          ? element.classList.remove("filter")
+          : element.classList.add("filter")
+      );
     }
+  });
+}
+
+export function filtrorol(selector, tank) {
+  //console.log(typeof entrandotanque);
+  document
+    .querySelectorAll(selector)
+    .forEach((element) =>
+      tank.includes(element.textContent)
+        ? element.classList.remove("filter")
+        : element.classList.add("filter")
+    );
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import data from "./data/lol/lol.js";
 
 import { buscarCampeones } from "./data.js";
 
-import {filtrorol} from  './data.js';
+import { filtrorol } from "./data.js";
 
 const contenido = document.getElementById("cartas");
 
@@ -19,43 +19,41 @@ for (var campeones in data.data) {
   name.innerText = campeon.name;
   div.appendChild(name);
   div.appendChild(imagenCampeon);
-  div.onclick = function(){
+  div.onclick = function () {
     mostrarPopup(campeon);
-};
+  };
 
   contenido.appendChild(div);
-
 }
 
 //ventana emergente al seleccionar campeon para mirar caracteristicas
-function mostrarPopup (campeon){
-    const contenido = document.getElementById("popup");
-    const name = document.createElement("h1");
-    const image = document.createElement("img");
-    const div = document.createElement("div");    
-    div.id=('popupdiv');   
-    name.innerText = campeon.id;
-    image.src = campeon.splash;
-    div.appendChild(name);
-    div.appendChild(image);
-    contenido.appendChild(div);
-    }
+function mostrarPopup(campeon) {
+  const contenido = document.getElementById("popup");
+  const name = document.createElement("h1");
+  const image = document.createElement("img");
+  const div = document.createElement("div");
+  div.id = "popupdiv";
+  name.innerText = campeon.id;
+  image.src = campeon.splash;
+  div.appendChild(name);
+  div.appendChild(image);
+  contenido.appendChild(div);
+}
 
-    
-    //function cerrarPopup (){
-        //const contenido = document.getElementById("popup");             
-      //contenido.innerHTML = '';       
-    
-    //}
+//function cerrarPopup (){
+//const contenido = document.getElementById("popup");
+//contenido.innerHTML = '';
 
+//}
 
 //Funcion donde usamos la barra de busqueda para hallar al campeon especifico
 buscarCampeones(".card-filter", ".card");
 
-
 //Funcion para filtrar por roles y demas a los campeones
 var roles = document.getElementsByClassName("roles");
+
 var roleslista = "";
+
 for (let i = 0; i < roles.length; i++) {
   roles[i].addEventListener("click", () => {
     if (roles[i].textContent == "Apoyo") {
@@ -71,6 +69,7 @@ for (let i = 0; i < roles.length; i++) {
     } else if (roles[i].textContent == "Tanque") {
       roleslista = "Tank";
     }
+
     var listaderoles = [];
     for (var rep in data.data) {
       //console.log(data.data[rep].tags);
@@ -81,35 +80,95 @@ for (let i = 0; i < roles.length; i++) {
         listaderoles.push(data.data[rep].name);
       }
     }
+    filtrorol(".card", listaderoles);
+  });
+}
 
-    filtrorol (".card", listaderoles);
+var contenedorgrande = [];
+
+var listadecampeones = {};
+
+for (var r in data.data) {
+  var nombre = data.data[r].name;
+  var ataque = data.data[r].stats.attackdamage;
+  var nuevalista = new listadecampeones(nombre, ataque);
+  contenedorgrande.push(nuevalista);
+}
+//console.log(contenedorgrande);
+var toplaner = document.getElementsByClassName("posiciones");
+
+for (let i = 0; i < toplaner.length; i++) {
+  toplaner[i].addEventListener("click", () => {
+    var listamaxattack = [];
+    for (var campeones in data.data) {
+      listamaxattack.push(data.data[campeones].stats.attackdamage);
+      var maximo = Math.max.apply(null, listamaxattack);
+    }
+
+    //console.log(listamaxattack);
+    //console.log(maximo);
+    var valores = [];
+
+    valores = listamaxattack.filter((item) => {
+      return item > maximo * 0.95 || item == 60;
+    });
+
+    console.log(valores);
+    var listadeposiciones = [];
+    if (toplaner[i].textContent == "Toplaner") {
+      for (var a in data.data) {
+        if (
+          (data.data[a].tags.includes("Tank") &&
+            data.data[a].stats.attackdamage < 0.95 * maximo) ||
+          data.data[a].tags.includes("Fighter")
+        ) {
+          listadeposiciones.push(data.data[a].name);
+        }
+      }
+      //console.log(listadeposiciones);
+      //console.log(listadeposiciones.length);
+    }
   });
 }
 
 
 const botonabrir = document.getElementById("menudesple");
-botonabrir.addEventListener("mouseover", abrir1);
-botonabrir.addEventListener("mouseout", cerrar1);
+botonabrir.addEventListener("mouseover", abrir);
+botonabrir.addEventListener("mouseout", cerrar);
 
-const posicion = document.getElementById("posiciones");
-posicion.addEventListener("click", abrir2);
-posicion.addEventListener("click", cerrar2);
-function abrir1() {
+const posiciones2 = document.getElementById("posiciones2");
+posiciones2.addEventListener("mouseover", abrir2);
+posiciones2.addEventListener("mouseout", cerrar2);
+
+const posiciones1 = document.getElementById("posiciones1");
+posiciones1.addEventListener("mouseover", abrir1);
+posiciones1.addEventListener("mouseout", cerrar1);
+
+function abrir() {
   var elem = document.getElementById("desplegables");
   elem.style.display = "block";
 }
 
-function cerrar1() {
+function cerrar() {
   var elem = document.getElementById("desplegables");
   elem.style.display = "none";
 }
 
-function abrir2() {
+function abrir1() {
   var elem = document.getElementById("subitem1");
   elem.style.display = "block";
 }
 
-function cerrar2() {
+function cerrar1() {
   var elem = document.getElementById("subitem1");
+  elem.style.display = "none";
+}
+function abrir2() {
+  var elem = document.getElementById("subitem2");
+  elem.style.display = "block";
+}
+
+function cerrar2() {
+  var elem = document.getElementById("subitem2");
   elem.style.display = "none";
 }
