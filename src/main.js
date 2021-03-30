64461a723ee1c47c2200b1398ea52bd84de11950
Angelica <atheran082@gmail.com>
2021-03-30 18:25:13 -0500

    organizando modulos

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import { buscarCampeones } from "./data.js";
 
 import { filtrorol } from "./data.js";
 
+import {listaRoles } from "./data.js";
+
+import {ListaAtaque} from "./data.js";
+
+import {ListaPosisiones} from "./data.js";
+
 const contenido = document.getElementById("cartas");
 
 //Funcion para mostrar campeones
@@ -49,87 +55,7 @@ function mostrarPopup(campeon) {
 //Funcion donde usamos la barra de busqueda para hallar al campeon especifico
 buscarCampeones(".card-filter", ".card");
 
-//Funcion para filtrar por roles y demas a los campeones
-var roles = document.getElementsByClassName("roles");
 
-var roleslista = "";
-
-for (let i = 0; i < roles.length; i++) {
-  roles[i].addEventListener("click", () => {
-    if (roles[i].textContent == "Apoyo") {
-      roleslista = "Support";
-    } else if (roles[i].textContent == "Asesino") {
-      roleslista = "Assassin";
-    } else if (roles[i].textContent == "Luchador") {
-      roleslista = "Fighter";
-    } else if (roles[i].textContent == "Tirador") {
-      roleslista = "Marksman";
-    } else if (roles[i].textContent == "Mago") {
-      roleslista = "Mage";
-    } else if (roles[i].textContent == "Tanque") {
-      roleslista = "Tank";
-    }
-
-    var listaderoles = [];
-    for (var rep in data.data) {
-      //console.log(data.data[rep].tags);
-      //includes verifica si dentro de un array hay un elemento
-      //que tenga la palabra que le meti xd
-      if (data.data[rep].tags.includes(roleslista)) {
-        //console.log(data.data[rep].name);
-        listaderoles.push(data.data[rep].name);
-      }
-    }
-    filtrorol(".card", listaderoles);
-  });
-}
-
-var contenedorgrande = [];
-
-var listadecampeones = {};
-
-for (var r in data.data) {
-  var nombre = data.data[r].name;
-  var ataque = data.data[r].stats.attackdamage;
-  var nuevalista = new listadecampeones(nombre, ataque);
-  contenedorgrande.push(nuevalista);
-}
-//console.log(contenedorgrande);
-var toplaner = document.getElementsByClassName("posiciones");
-
-for (let i = 0; i < toplaner.length; i++) {
-  toplaner[i].addEventListener("click", () => {
-    var listamaxattack = [];
-    for (var campeones in data.data) {
-      listamaxattack.push(data.data[campeones].stats.attackdamage);
-      var maximo = Math.max.apply(null, listamaxattack);
-    }
-
-    //console.log(listamaxattack);
-    //console.log(maximo);
-    var valores = [];
-
-    valores = listamaxattack.filter((item) => {
-      return item > maximo * 0.95 || item == 60;
-    });
-
-    console.log(valores);
-    var listadeposiciones = [];
-    if (toplaner[i].textContent == "Toplaner") {
-      for (var a in data.data) {
-        if (
-          (data.data[a].tags.includes("Tank") &&
-            data.data[a].stats.attackdamage < 0.95 * maximo) ||
-          data.data[a].tags.includes("Fighter")
-        ) {
-          listadeposiciones.push(data.data[a].name);
-        }
-      }
-      //console.log(listadeposiciones);
-      //console.log(listadeposiciones.length);
-    }
-  });
-}
 
 
 const botonabrir = document.getElementById("menudesple");
