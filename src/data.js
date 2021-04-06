209040f546eb138600286fd58143d06af76496a7
Angelica <atheran082@gmail.com>
2021-04-06 18:42:45 -0500

    funcionalidad barra de busqueda y botones de categorias

--- src/data.js
+++ src/data.js
@@ -1,2 +1,25 @@
 
+
+export function filtrorol (selector,tank) {
+//console.log( typeof entrandotanque)
+document.querySelectorAll(selector).forEach((element) => 
+tank.includes(element.textContent ) 
+?element.classList.remove("filter")
+:element.classList.add("filter")
+)
+}
+
+export function filtrotop (selector,top) {
+  document.querySelectorAll(selector).forEach((element) => 
+  top.includes(element.textContent ) 
+  ?element.classList.remove("filter")
+  :element.classList.add("filter"))
+  }
+
+  export function filtrojungla (selector,jungla) {
+      document.querySelectorAll(selector).forEach((element) => 
+      jungla.includes(element.textContent ) 
+      ?element.classList.remove("filter")
+      :element.classList.add("filter"))
+      }
  
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import data from "./data/lol/lol.js";
+
 const dataCampeones = document.querySelector("#fichasCampeon");
 
 
 
 //Funcion para mostrar campeones
-
 for (var campeones in data.data) {
   const campeon = data.data[campeones];
   const div = document.createElement("div");
@@ -23,8 +23,69 @@ for (var campeones in data.data) {
   dataCampeones.appendChild(div);
 }
 
+//se crean filtros paraa buscar por nombre especifico al personaje
+
+function buscarCampeones (input,selector) {
+  //utilizar metodos aca de los puros para filter
+  //y esto es lo que hay que testear
+  document.addEventListener("keyup",(e) =>{
+      //target funciona pa saber cuando avtive el evento jiji
+       
+      if(e.target.matches(input)){
+          //matches verifica que la clase sea la misma
+          //element generalizacion para representar un objeto en html y sacar cositas de ahi jiji xd
+          document.querySelectorAll(selector).forEach((element) => 
+          //tolower pasa todo a minuscula pa que sea mas sencillo
+           element.textContent.toLowerCase().includes(e.target.value)
+           //despues del signo de pregunta es como el true :D si la condicion de arriba se cumple
+          ?element.classList.remove("filter")
+          :element.classList.add("filter")
+          );                  
+      }      
+  });    
+}
+buscarCampeones(".barra-busqueda", ".card");
 
 
+//se crea funcion para filtrar por roles
+function filtrorol (selector,tank) {
+  //console.log( typeof entrandotanque)
+  document.querySelectorAll(selector).forEach((element) => 
+  tank.includes(element.textContent ) 
+  ?element.classList.remove("filter")
+  :element.classList.add("filter")
+  )
+  }
+var categorias = document.getElementsByClassName("categorias");
+var categorylist = "";
+for (let i = 0; i < categorias.length; i++) {
+  categorias[i].addEventListener("click", () => {
+    if (categorias[i].value == "Apoyo") {
+      categorylist = "Support";
+    } else if (categorias[i].value == "Asesino") {
+      categorylist = "Assassin";
+    } else if (categorias[i].value == "Luchador") {
+      categorylist = "Fighter";
+    } else if (categorias[i].value == "Tirador") {
+      categorylist = "Marksman";
+    } else if (categorias[i].value == "Mago") {
+      categorylist = "Mage";
+    } else if (categorias[i].value == "Tanque") {
+      categorylist = "Tank";
+    }
+    var listCategory = [];
+    for (var rep in data.data) {
+      //console.log(data.data[rep].tags);
+      //includes verifica si dentro de un array hay un elemento
+      //que tenga la palabra que le meti xd
+      if (data.data[rep].tags.includes(categorylist)) {
+        //console.log(data.data[rep].name);
+        listCategory.push(data.data[rep].name);
+      }
+    }
+    filtrorol(".card", listCategory);
+  });
+}
 
 
 //ventana emergente al seleccionar campeon para mirar caracteristicas
